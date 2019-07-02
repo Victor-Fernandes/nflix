fd85cca77abcc45e94e575bfc2a1367fb0d68502
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-07-02 17:37:49 -0300

    Adicionando tempo de espera por elementos

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,4 +8,5 @@ World(Helpers) #indica q todos o metodos do modulo Helpers são nativos
 Capybara.configure do |config|
   config.default_driver = :selenium
   config.app_host = "http://localhost:8080"
+  config.default_max_wait_time = 5
 end
