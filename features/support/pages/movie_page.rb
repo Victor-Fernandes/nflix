abdd3e66982351f33c3f392397c2e87c6f46c6de
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-07-03 18:46:17 -0300

    terminando de inserir dados no formulario

--- features/support/pages/movie_page.rb
+++ features/support/pages/movie_page.rb
@@ -5,6 +5,26 @@ class MoviePage
         find(".nc-simple-add").click
     end
     
+    def upload(file)
+        #upload cover img, element it's hidden. Configure capybara for find element hiden 
+        cover_file = File.join(Dir.pwd, "features/support/fixtures/cover/" + file)
+
+        cover_file = cover_file.tr("/", "\\") if OS.windows? #se for windows ele inverte as barras
+ 
+        Capybara.ignore_hidden_elements = false
+        attach_file('upcover', cover_file)
+        Capybara.ignore_hidden_elements = true
+    end
+    
+    def add_cast(cast)
+        #add cast
+        actor = find(".input-new-tag")
+        cast.each do |a|
+            actor.set a
+            actor.send_keys :tab
+        end
+    end
+
     def create(movie)
         find("input[name=title]").set movie["title"]
 
@@ -15,12 +35,12 @@ class MoviePage
         find("input[name=year]").set movie["year"]
         find("input[name=release_date]").set movie["release_date"]
 
-        actor = find(".input-new-tag")
-        movie['cast'].each do |a|
-            actor.set a
-            actor.send_keys :tab 
-        end
+        add_cast(movie["cast"])
 
         find("textarea[name=overview]").set movie["overview"]
+
+       upload(movie["cover"])
+
+        click_button "Cadastrar"
     end
 end
