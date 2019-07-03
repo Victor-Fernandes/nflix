75222b9a0f3390d21932dbc8072f54f321835ea6
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-07-03 18:44:56 -0300

    importando os

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,15 @@
 require "capybara"
 require "capybara/cucumber"
+require "os"
 require "selenium-webdriver"
 require_relative "helpers"
 
 World(Helpers) #indica q todos o metodos do modulo Helpers são nativos
 
 Capybara.configure do |config|
-  config.default_driver = :selenium
+  config.default_driver = :selenium_chrome #usando o chrome pois o firefox não consegue encontrar o elemento 
+  #para selecionar o status do filme
+  #config.default_driver = :selenium
   config.app_host = "http://localhost:8080"
   config.default_max_wait_time = 5
 end
