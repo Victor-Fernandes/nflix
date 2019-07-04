b81e7f08b2b76884d5046a6ba4668f6981255e8d
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-07-04 04:36:12 -0300

    aumentando o timeout para gerar o token

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -1,8 +1,14 @@
 module Helpers
 
     def getToken
-        # pegando o token de sessão do usuario
-        js_script = 'return window.localStorage.getItem("default_auth_token");'
-        page.execute_script(js_script) # executando comando js
+        timeout = Capybara.default_max_wait_time
+        #codigo para esperar por mais tempo pela geração do token
+        timeout.times do
+            # pegando o token de sessão do usuario
+            js_script = 'return window.localStorage.getItem("default_auth_token");'
+            @token = page.execute_script(js_script) # executando comando js
+            sleep 1
+        end
+        @token
     end
 end
