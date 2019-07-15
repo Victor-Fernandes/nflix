904b9e70b1b45a6a1f76e3f672f194ff8ce6255d
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-07-15 15:57:19 -0300

    carregando arquivo de configuração

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,10 +6,13 @@ require_relative "helpers"
 
 World(Helpers) #indica q todos o metodos do modulo Helpers são nativos
 
+#Carregando arquivo de configuração de ambiente
+CONFIG =  YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
+
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome #usando o chrome pois o firefox não consegue encontrar o elemento 
   #para selecionar o status do filme
   #config.default_driver = :selenium
-  config.app_host = "http://localhost:8080"
+  config.app_host = CONFIG["url"]
   config.default_max_wait_time = 5
 end
